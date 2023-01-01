0e0e9bfd66f345c1e3b5950b022bb9a815627e57
michael-stac <mickymouze778@gmail.com>
2023-01-01 13:07:39 +0100

    added service db

--- lib/Screens/create_note.dart
+++ lib/Screens/create_note.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
+import '../Service/db_helper.dart';
 import '../Style/app_color.dart';
+import '../Util/router.dart';
 
 class CreateNote extends StatefulWidget {
   const CreateNote({Key? key}) : super(key: key);
@@ -10,9 +12,27 @@ class CreateNote extends StatefulWidget {
 }
 
 class _CreateNoteState extends State<CreateNote> {
+  GlobalKey<FormState> key = GlobalKey();
+  TextEditingController title = TextEditingController();
+  TextEditingController body = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: AppColor.primaryColor,
+          onPressed: () {
+            if (key.currentState!.validate()) {
+              ///Save dat to firebase
+              DbHelper()
+                  .add(title: title.text.trim(), body: body.text.trim())
+                  .then((value) {
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text(value)));
+              });
+            }
+          },
+          label: Text("Save note", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w700, color: AppColor.white),)),
+
       backgroundColor: AppColor.white,
 
       appBar: AppBar(
@@ -21,6 +41,74 @@ class _CreateNoteState extends State<CreateNote> {
         leading: BackButton(color: AppColor.black, ),
 
         title: Text("Create Note", style: TextStyle(fontWeight: FontWeight.w700, fontSize: 22, color: AppColor.primaryColor),),
+      ),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 19, vertical: 20),
+              child: Form(
+                key: key,
+                child: Column(
+                  children: [
+                    ///Title note
+                    TextFormField(
+
+                      controller: title,
+                      decoration:  InputDecoration(
+                          contentPadding: EdgeInsets.all(8),
+
+                        label: Text('Note title'),
+                        labelStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 16, color: AppColor.black),
+
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: AppColor.primaryColor)
+                        ),
+                        border: OutlineInputBorder(
+                            borderSide: BorderSide(color: AppColor.primaryColor)
+                        ),
+                      ),
+                      validator: (value) {
+                        if (value!.isEmpty) {
+                          return 'Title is empty';
+                        }
+                      },
+                    ),
+
+                    const SizedBox(height: 30),
+
+                    ///Description
+                    ///
+                    TextFormField(
+
+                        controller: body,
+                        maxLines: 7,
+                        minLines: 2,
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'body is empty';
+                          }
+                        },
+                        decoration:  InputDecoration(
+                          contentPadding: EdgeInsets.all(8),
+                            label: const Text('Note body'),
+                            labelStyle: TextStyle(fontWeight: FontWeight.w700, fontSize: 16, color: AppColor.black),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: AppColor.primaryColor)
+                            ),
+                            border: OutlineInputBorder(
+                              borderSide: BorderSide(color: AppColor.primaryColor)
+                            ),
+
+
+                        )
+                    )
+                  ],
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
